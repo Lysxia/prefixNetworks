c9348a4818281a7ae13e629441ad20f9dbb0350c
Syrak <li-yao.xia@ens.fr>
2014-03-23 15:12:33 +0100

    Update notice

--- Memo.hs
+++ Memo.hs
@@ -25,6 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --}
 
+-- Updated for GHC 7.9, March 2014, on github.com/Syrak
 
 
 {-# LANGUAGE DeriveGeneric, DefaultSignatures, FlexibleContexts #-}
